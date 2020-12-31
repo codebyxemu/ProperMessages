7513ca56526694a6537e12337fcb96d27172f82b
Code By Xemu <74688700+codebyxemu@users.noreply.github.com>
2020-12-31 19:13:54 +0100

    Init

---
+++ src/main/java/me/xemu/propermessages/ProperMessages.java
@@ -0,0 +1,34 @@
+// Code by Xemu (https://www.github.com/codebyxemu)
+package me.xemu.propermessages;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ProperMessages extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    ;
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    ;
+
+    private final void loadCommands() {
+
+    }
+
+    ;
+
+    private final void loadEvents() {
+
+    }
+
+    ;
+
+}
